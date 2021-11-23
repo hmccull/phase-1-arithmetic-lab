318856439d6c2de8fbd624e7f45d1ef5ae61f4dc
Hannah McCullough <hmac091@gmail.com>
2021-11-22 17:46:05 -0700

    commit tree 548ea1d63447ff43ebd4ecff1f4c411390c94a78

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// two numbers multiplied to equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+// generate random number greater than 0
+let random = Math.ceil(Math.random());
+
+// divide two numbers with the remainder of 4
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+
+// find highest number in a set, returned value should be 20
+let max = Math.max(1, 2, 4, 7, 20);
